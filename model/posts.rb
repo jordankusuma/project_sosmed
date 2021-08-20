b0c24d0f64bf2bba99a9f5c6a9d678694f97b32f
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 21:10:25 +0700

    [Green][FEATURE] add get_post_by_hashtag method in Posts with status message

--- model/posts.rb
+++ model/posts.rb
@@ -13,6 +13,30 @@ class Posts
         @date = params[:date]
     end
 
+    #filter hashtag to get all posts 
+    def self.get_post_by_hashtag(params)
+        client = create_db_client 
+        rawData = client.query("select posts.id, posts.user_id, posts.date, posts.post_text, posts.attachment from posts join posts_hashtag on posts.id = posts_hashtag.post_id join hashtags on posts_hashtag.hashtag_id = hashtags.id where hashtags.name = '#{params.downcase}'")
+        posts = Array.new 
+        hash = Hash.new
+        rawData.each do |data|
+            hash = {:id => data["id"], :user_id => data["user_id"], :date => data["date"], :post_text => data["post_text"], :attachment => data["attachment"]}
+            posts << hash
+        end
+        if (!posts.empty?)
+            {
+                'status' => 200,
+                'message' => 'Success',
+                'data' => posts
+            }
+        else 
+            {
+                'status' => 404,
+                'message' => 'Not found Post Based on hashtags name'
+            }
+        end
+    end
+
     #check if value is nil
     def valid?
         return false if @user_id.nil?   
