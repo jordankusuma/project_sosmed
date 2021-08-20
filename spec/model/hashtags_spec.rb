25bf1fc0345bad366f396ea6e83f9e2b088e3943
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 22:58:40 +0700

    [Red][TEST] add test for initialize hashtags to return true when valid

---
+++ spec/model/hashtags_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../../model/hashtags'
+
+describe Hashtags do
+    before :each do
+        @stub_client = double
+        allow(Mysql2::Client).to receive(:new).and_return(@stub_client)
+        @hashtag = Hashtags.new({id: 1, name: "aku", quantity: 0})
+        @hashtag_nil = Hashtags.new({id: 1, name: nil, quantity: 0})
+        @hashtags = [
+            {'id': 1, 'name': "aku", 'quantity': 0}
+        ]
+        @response = {'id' => 1, 'name' => "aku", 'quantity' => 0}
+    end
+
+    describe 'initialize' do
+        context 'when initialize' do
+            it 'return true when valid' do
+                expect(@hashtag.valid?).to eq(true)
+            end
+        end 
+
+    end
