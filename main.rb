176d3af4c384ab1faaebdfa90b6ac4bf37dd3d44
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 21:54:20 +0700

    [FEATURE] add method get filter post by hashtag name and method post for creating new posts

--- main.rb
+++ main.rb
@@ -7,9 +7,9 @@ require './controller/HashtagController'
 require 'sinatra/json'
 
 ## Custom Method for Getting Request body
-def getBody (req)
+def getBody(req)
   req.body.rewind
-  return JSON.parse(req.body.read)
+  JSON.parse(req.body.read)
 end
 
 namespace '/api/v1' do
@@ -17,15 +17,38 @@ namespace '/api/v1' do
     content_type :json
   end
 
-  #Users
-  #register
+  # Users
+  # register
   post '/users/register' do
     body = getBody(request)
     controller = UserController.new
-		object = controller.register(body)
+    object = controller.register(body)
     object.to_json
   end
-  #End Users
+  # End Users
 
-  
+  # posts
+  post '/posts/new' do
+    @filename = params['files']['filename']
+    file = params['files']['tempfile']
+
+    File.open("./uploadsPosts/#{@filename}", 'w') do |f|
+      f.write(file.read)
+    end
+
+    controller = PostController.new
+    object = controller.save(params, @filename)
+    object.to_json
+  end
+
+  # get post based on hashtag name
+  get '/posts/hashtags/{name}' do
+    controller = PostController.new
+    # name = '#' + params["name"].to_s
+    name = params['name'].to_s
+    object = controller.get_post_by_hash(name)
+    # # object.to_json+"\n"+posts.to_json
+    object.to_json
+  end
+  # end posts
 end
