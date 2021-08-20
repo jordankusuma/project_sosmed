e34ed353fc3aae221a232fc18879d77488d38f24
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 21:58:05 +0700

    [Red][TEST] add test for initialize to return true when valid Comments

---
+++ spec/model/comments_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../../model/comments'
+require_relative '../../model/hashtags'
+
+describe Comments do
+    before :each do
+        @stub_client = double
+        allow(Mysql2::Client).to receive(:new).and_return(@stub_client)
+        @comment = Comments.new({id: 1, post_id: 2, comments_text: "saya", attachment: "a.png", user_id: 1, date: '2021-08-17 17:04:54'})
+        @comments_attach_nil = Comments.new({id: 1, post_id: 2, comments_text: "saya", attachment: nil, user_id: 1, date: '2021-08-17 17:04:54'})
+        @comments = [
+            {'id': 1, 'post_id': 2, 'comments_text': "saya", 'attachment': "a.png", 'user_id': 1, 'date': '2021-08-17 17:04:54'}
+        ]
+        @response = {'id' => 1, 'post_id' => 2, 'comments_text' => "saya", 'attachment' => "a.png", 'user_id' => 1, 'date' => '2021-08-17 17:04:54'}
+    end
+
+    describe 'initialize' do
+        context 'when initialize' do
+            it 'return true when valid' do
+                expect(@comment.valid?).to eq(true)
+            end
+
+        end
+    end 
+end
