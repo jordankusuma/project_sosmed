d8f42c700781ce7cfe718b0a1f720b34d39d5275
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 20:36:13 +0700

    [Green][FEATURE] add register method in Users class

--- model/users.rb
+++ model/users.rb
@@ -10,6 +10,19 @@ class Users
     @bio = params[:bio]
   end
 
+  #create new account
+  def register
+    client = create_db_client 
+    return false unless valid?
+    client.query("INSERT INTO users VALUES (0, '#{@username}', '#{@email}', '#{@bio}')")
+    
+    id_new = client.last_id()
+    response = client.query("SELECT * FROM users WHERE id = #{id_new}")
+
+    data = response
+    id_new
+  end
+
   # check if value is nil
   def valid?
     if @bio.nil? || @email.nil? || @username.nil? || !@email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
