d86388eb5251ff4aa7fd75ffbf293ddb5d56b587
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 23:23:08 +0700

    [Green][FEATURE] add get_trending_hashtags in Hashtags to get trending Hashtags

--- model/hashtags.rb
+++ model/hashtags.rb
@@ -49,4 +49,27 @@ class Hashtags
         client.query("update hashtags set quantity = 0")
         true
     end
+
+    def self.get_trending_hashtag
+        client = create_db_client 
+        rawData = client.query("select * from hashtags order by quantity desc limit 5")
+        hashtags = Array.new 
+        hash = Hash.new
+        rawData.each do |data|
+            hash = {:id => data['id'], :name => data['name'], :quantity => data['quantity']}
+            hashtags << hash
+        end
+        if (!hashtags.empty?)
+            {
+                'status' => 200,
+                'message' => 'Success',
+                'data' => hashtags
+            }
+        else 
+            {
+                'status' => 404,
+                'message' => 'Not found post/comments in 24 hours',
+            }
+        end
+    end
 end
