4a62d2024634ff11cfb1dd00b31cdf217fcfebde
jordankusuma <c14180102@john.petra.ac.id>
2021-08-20 20:34:22 +0700

    [DEPEDENCIES] Use Rubocop auto correct for indentation

--- model/users.rb
+++ model/users.rb
@@ -1,18 +1,21 @@
 require_relative '../db/db_connector'
 
 class Users
-    attr_accessor :id, :username, :email, :bio
+  attr_accessor :id, :username, :email, :bio
 
-    def initialize(params)
-        @id = params[:id]
-        @username = params[:username]
-        @email = params[:email]
-        @bio = params[:bio]
+  def initialize(params)
+    @id = params[:id]
+    @username = params[:username]
+    @email = params[:email]
+    @bio = params[:bio]
+  end
+
+  # check if value is nil
+  def valid?
+    if @bio.nil? || @email.nil? || @username.nil? || !@email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+      return false
     end
 
-    #check if value is nil
-    def valid?
-        return false if @bio.nil? || @email.nil? || @username.nil? || !(@email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i))
-        true
-    end
+    true
+  end
 end
--- spec/model/users_spec.rb
+++ spec/model/users_spec.rb
@@ -1,64 +1,64 @@
 require_relative '../../model/users'
 
 describe Users do
-    before :each do
-        @stub_client = double()
-        allow(Mysql2::Client).to receive(:new).and_return(@stub_client)
-        @user = Users.new({id: 0, username: "jo", email: "a@gmail.com", bio: "saya"})
-        @response = { 'id' => 0, 'username' => 'jo', 'email' => 'a@gmail.com', 'bio' => 'saya' }
+  before :each do
+    @stub_client = double
+    allow(Mysql2::Client).to receive(:new).and_return(@stub_client)
+    @user = Users.new({ id: 0, username: 'jo', email: 'a@gmail.com', bio: 'saya' })
+    @response = { 'id' => 0, 'username' => 'jo', 'email' => 'a@gmail.com', 'bio' => 'saya' }
+  end
+  describe 'initialize' do
+    context 'when initialize' do
+      it 'return true when valid' do
+        expect(@user.valid?).to eq(true)
+      end
+      it 'return false when not valid' do
+        @user = Users.new({ id: 0, username: 'jo' })
+        expect(@user.valid?).to eq(false)
+      end
     end
-    describe 'initialize' do
-        context 'when initialize' do
-            it 'return true when valid' do
-                expect(@user.valid?).to eq(true)
-            end
-            it 'return false when not valid' do
-                @user = Users.new({id: 0, username: "jo"})
-                expect(@user.valid?).to eq(false)
-            end
-        end 
-    end
+  end
 
-    describe 'register' do
-        context 'when register' do 
-            it 'should register when valid' do
-                query = "INSERT INTO users VALUES (0, '#{@user.username}', '#{@user.email}', '#{@user.bio}')"
-                query1 = "SELECT * FROM users WHERE id = #{@user.id}"
-                expect(@stub_client).to receive(:query).with(query)                
-                allow(@stub_client).to receive(:last_id).and_return(@user.id)
-                expect(@stub_client).to receive(:query).with(query1).and_return([@response]) 
-                expect(@user.register).to eq(@user.id)     
-            end
-            it 'should not register when not valid' do
-                @user = Users.new({id: 0, username: "jo"})             
-                expect(@user.register).to eq(false)
-            end
-        end 
+  describe 'register' do
+    context 'when register' do
+      it 'should register when valid' do
+        query = "INSERT INTO users VALUES (0, '#{@user.username}', '#{@user.email}', '#{@user.bio}')"
+        query1 = "SELECT * FROM users WHERE id = #{@user.id}"
+        expect(@stub_client).to receive(:query).with(query)
+        allow(@stub_client).to receive(:last_id).and_return(@user.id)
+        expect(@stub_client).to receive(:query).with(query1).and_return([@response])
+        expect(@user.register).to eq(@user.id)
+      end
+      it 'should not register when not valid' do
+        @user = Users.new({ id: 0, username: 'jo' })
+        expect(@user.register).to eq(false)
+      end
     end
+  end
 
-    describe 'valid?' do
-        context 'validation' do 
-            it 'return true if valid' do 
-                expect(@user.valid?).to eq(true)
-            end
-        end
-        context 'when not valid' do
-            it 'return false when username is nil' do
-                @users = Users.new({id: 0, username: nil, email: 'a@gmail.com', bio: 'Saya'})
-                expect(@users.valid?).to eq(false)
-            end
-            it 'return false when email is nil' do
-                @users = Users.new({id: 0, username: 'jo', email: nil, bio: 'Saya'})
-                expect(@users.valid?).to eq(false)
-            end
-            it 'return false when email is invalid' do
-                @users = Users.new({id: 0, username: 'jo', email: 'a', bio: 'Saya'})
-                expect(@users.valid?).to eq(false)
-            end
-            it 'should return false when bio is nil' do
-                @users = Users.new({id: 0, username: 'jo', email: 'a@gmail.com', bio: nil})
-                expect(@users.valid?).to eq(false)
-            end
-        end
+  describe 'valid?' do
+    context 'validation' do
+      it 'return true if valid' do
+        expect(@user.valid?).to eq(true)
+      end
     end
-end 
+    context 'when not valid' do
+      it 'return false when username is nil' do
+        @users = Users.new({ id: 0, username: nil, email: 'a@gmail.com', bio: 'Saya' })
+        expect(@users.valid?).to eq(false)
+      end
+      it 'return false when email is nil' do
+        @users = Users.new({ id: 0, username: 'jo', email: nil, bio: 'Saya' })
+        expect(@users.valid?).to eq(false)
+      end
+      it 'return false when email is invalid' do
+        @users = Users.new({ id: 0, username: 'jo', email: 'a', bio: 'Saya' })
+        expect(@users.valid?).to eq(false)
+      end
+      it 'should return false when bio is nil' do
+        @users = Users.new({ id: 0, username: 'jo', email: 'a@gmail.com', bio: nil })
+        expect(@users.valid?).to eq(false)
+      end
+    end
+  end
+end
