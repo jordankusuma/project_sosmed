ffab0b0abcb9fde4b7c4bf57ecd9403138694a8e
jordankusuma <c14180102@john.petra.ac.id>
2021-08-21 16:19:02 +0700

    [Green][FEATURE] add method to get comments when params valid HashtagController

--- controller/HashtagController.rb
+++ controller/HashtagController.rb
@@ -12,7 +12,19 @@ class HashtagController
             false
         end
     end
-    
+
+    def get_comment(params)
+        if (params != nil)
+            data = Array.new 
+            params.each do |param|
+                data.push(param.comments_text)
+            end
+            data
+        else 
+            false
+        end
+    end
+
     def update_quantity(text) 
         text.each do |params|
             arr_hashtag = Hashtags.get_hashtag(params)
