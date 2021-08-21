1d8edc1c7491e98de3b8252dc59dce1c3006dc8b
jordankusuma <c14180102@john.petra.ac.id>
2021-08-21 16:14:35 +0700

    [Green][FEATURE] add method to get post text in HashtagController

--- controller/HashtagController.rb
+++ controller/HashtagController.rb
@@ -1,6 +1,18 @@
 require './model/hashtags'
 
 class HashtagController 
+    def get_post(params)
+        if (params != nil)
+            data = Array.new 
+            params.each do |param|
+                data.push(param.post_text)
+            end
+            data
+        else 
+            false
+        end
+    end
+    
     def update_quantity(text) 
         text.each do |params|
             arr_hashtag = Hashtags.get_hashtag(params)
