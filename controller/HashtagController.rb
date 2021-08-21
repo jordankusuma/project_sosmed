65cba33922434e37b3d27ffb8e98dc57a4e45406
jordankusuma <c14180102@john.petra.ac.id>
2021-08-21 16:23:35 +0700

    [Green][FEATURE] add method to get trending hashtag within 24 hours

--- controller/HashtagController.rb
+++ controller/HashtagController.rb
@@ -1,6 +1,26 @@
 require './model/hashtags'
 
 class HashtagController 
+    def get_trending_hashtag(posts, comments)
+        posts_text = get_post(posts)
+        comments_text = get_comment(comments)
+
+        if (posts_text != false && comments_text != false)
+            Hashtags.reset_quantity
+            #get post hashtag and update quantity
+            update_quantity(posts_text)
+            update_quantity(comments_text)
+
+            hashtags = Hashtags.get_trending_hashtag
+            hashtags
+        else 
+            {
+                'status' => 404,
+                'message' => 'Not found post/comments in 24 hours',
+            }
+        end
+    end
+    
     def get_post(params)
         if (params != nil)
             data = Array.new 
