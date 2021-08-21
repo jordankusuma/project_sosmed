c6f8d9993a13dbc00bc8afd0d553dc7fb66fccfe
jordankusuma <c14180102@john.petra.ac.id>
2021-08-21 16:06:36 +0700

    [Green][FEATURE] add save method to save comments CommentController

--- controller/CommentController.rb
+++ controller/CommentController.rb
@@ -4,6 +4,41 @@ require './model/posts'
 require './model/comments'
 
 class CommentController 
+    def save(params, filename)
+        arr_hashtag = Hashtags.get_hashtag(params['text'])
+        comments = Comments.new({"id": 0, "post": params['post_id'], "text": params['text'], "attachment": filename, "users": params['user_id'], "hashtag": arr_hashtag})
+        result = comments.save
+
+        if result == false 
+            {
+                'status' => 500,
+                'message' => 'Error',
+            }
+        else 
+            comment = Comments.get_comment(result) 
+
+            arr_hashtag.each do |data|
+                hashtag = Hashtags.new({name: data})
+                hashtag.save_hashtags
+                hashtag.save_commentshashtag(result)
+            end
+
+            {
+                'status' => 200,
+                'message' => 'Success',
+                'data' => {
+                    'id' => result,
+                    'post_id' => comment.post_id,
+                    'user_id' => comment.user_id,
+                    'comments_text' => comment.comments_text,
+                    'date' => comment.date,
+                    'attachment' => comment.attachment,
+                    'hashtag' => arr_hashtag
+                }
+            }
+        end
+    end
+    
     def get_comments(params)
         comment = Comments.get_all_comments(params) 
         comment unless comment.nil?
